c73ecc541dd60a484482667d49634abbf15927fd
jalhadab <67709677+jalhadab@users.noreply.github.com>
2020-07-11 06:09:07 +0300

    updated links to lab 1

--- ar.php
+++ ar.php
@@ -39,7 +39,7 @@
 		</section>
 		
 		<footer>
-			<p>CSC 174: Advanced Front-end Web Design and Development -<a href="http://csc174.org/lab01/jalhadab/" target=_blank>Original Lab 01</a></p>
+			<p>CSC 174: Advanced Front-end Web Design and Development -<a href="http://csc174.org/lab01/aramir10/" target=_blank>Original Lab 01</a></p>
 		</footer>
 
 	</div>
