701d744db41c4fd0d9a890ae2455e3abbb8d4994
jalhadab <67709677+jalhadab@users.noreply.github.com>
2020-07-11 04:11:07 +0300

    added content to jm.php

--- jm.php
+++ jm.php
@@ -1,36 +1,36 @@
 <?php include "inc/html-top.php"; ?>
 
-<body class="jaafar">
+<body class="jack">
 	<?php include "inc/nav.php"; ?>
 	<div class="background1">
 		<div class = "container1">
 			<header>
-				<figure class="personal-image"><img src="images/back1.jpg" alt="Jaafar Hadab"></figure>
-				<h1>Jaafar Hadab</h1>
+				<figure class="personal-image"><img src="images/face.png" alt="Jack Mandell"></figure>
+				<h1>Jack Mandell</h1>
 			</header>
 
 			<div class="intro-statement">
-				<p>I am a Mechanical Engineering Student at the University of Rochester. I am experienced in CAD modeling, Finite Element Analysis (FEA), and Manufacturing. I am also minoring in Computer Science and Psychology, from which I received experience in fundemental coding in Java and Python.</p>
+				<p>I am a Math and Computer Science double major studying at the University of Rochester. I am a rising sophomore, and outside of my majors, I enjoy passing the time with some of my favorite hobbies. These hobbies include art, cello, and fishing.</p>
 			</div>
 		</div>
 	</div>
 	<div class="container2">
 		<section class = "extracurricular1">
-			<h2>Research</h2>
-			<p>I currently work with the Dias laboratory group in the University of Rochester. The Dias group uses Diamond Anvil Cells (DACs) to study matter at extreme states. My current work with the group involves using Finite Element Models to study the shapes of the tips of diamonds in DACs in order to optimize for highest possible pressure.</p>
-			<figure class = "activity1"><img src="images/back2.jpg" alt="Jaafar Hadab"></figure>
+			<h2>Art</h2>
+			<p>For many years as a child, I took art lessons, and recently for the past two years over the summer, I have taught art lessons at studios near me. I enjoy painting the most and more specifically I like watercolor and acrylic. Many of my favorite things to paint include landscapes and still life.</p>
+			<figure class = "activity1"><img src="images/art.jpg" alt="art"></figure>
 		</section>
 
 		<section class = "extracurricular2">
-			<h2>Baja SAE</h2>
-			<p>I am a member of the University of Rochester Baja SAE team. Each year, we design and build an offroad vehicle to compete with other baja teams from all around the world. I currently serve as the suspension team.</p>
-			<figure class = "activity2"><img src="images/back3.jpg" alt="Jaafar Hadab"></figure>
+			<h2>Cello</h2>
+			<p>I have played cello for many years and enjoy listening to classical music. Wanting to continue playing cello on a consistent basis during college, I joined the University's Chamber Orchestra and have met people who appreciate classical as much as I do. I have even joined a small string ensemble with a few freinds for the upcoming semester.</p>
+			<figure class = "activity2"><img src="images/cello.png" alt="cello"></figure>
 		</section>
 
 		<section class = "extra-interest">
-			<h2>Personality Psychology</h2>
-			<p>I also have a keen interest in personality psychology, psychodynamic theories, and theories of development, and I am interested in how we can apply these ideas to our everyday lives.</p>
-			<figure class = "activity3"><img src="images/back4.jpg" alt="Jaafar Hadab"></figure>
+			<h2>Fishing</h2>
+			<p>I love to go fishing in the ponds around my home, as well as exploring new waters when I am traveling. I am drawn to the unpredicatability of fishing and the adventure of exploring new areas I have never fished before. Fishing has taught me a lot about patience and persistence. The feeling of fighting a large fish never gets old.</p>
+			<figure class = "activity3"><img src="images/fishing.png" alt="fishing"></figure>
 		</section>
 
 		<section class = "contact-info">
