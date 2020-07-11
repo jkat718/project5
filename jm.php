983523b9e39ae65eba8cbc571e9b10361f8e73bd
jalhadab <67709677+jalhadab@users.noreply.github.com>
2020-07-11 03:59:18 +0300

    created jm.php, added images for jm.php

---
+++ jm.php
@@ -0,0 +1,50 @@
+<?php include "inc/html-top.php"; ?>
+
+<body class="jaafar">
+	<?php include "inc/nav.php"; ?>
+	<div class="background1">
+		<div class = "container1">
+			<header>
+				<figure class="personal-image"><img src="images/back1.jpg" alt="Jaafar Hadab"></figure>
+				<h1>Jaafar Hadab</h1>
+			</header>
+
+			<div class="intro-statement">
+				<p>I am a Mechanical Engineering Student at the University of Rochester. I am experienced in CAD modeling, Finite Element Analysis (FEA), and Manufacturing. I am also minoring in Computer Science and Psychology, from which I received experience in fundemental coding in Java and Python.</p>
+			</div>
+		</div>
+	</div>
+	<div class="container2">
+		<section class = "extracurricular1">
+			<h2>Research</h2>
+			<p>I currently work with the Dias laboratory group in the University of Rochester. The Dias group uses Diamond Anvil Cells (DACs) to study matter at extreme states. My current work with the group involves using Finite Element Models to study the shapes of the tips of diamonds in DACs in order to optimize for highest possible pressure.</p>
+			<figure class = "activity1"><img src="images/back2.jpg" alt="Jaafar Hadab"></figure>
+		</section>
+
+		<section class = "extracurricular2">
+			<h2>Baja SAE</h2>
+			<p>I am a member of the University of Rochester Baja SAE team. Each year, we design and build an offroad vehicle to compete with other baja teams from all around the world. I currently serve as the suspension team.</p>
+			<figure class = "activity2"><img src="images/back3.jpg" alt="Jaafar Hadab"></figure>
+		</section>
+
+		<section class = "extra-interest">
+			<h2>Personality Psychology</h2>
+			<p>I also have a keen interest in personality psychology, psychodynamic theories, and theories of development, and I am interested in how we can apply these ideas to our everyday lives.</p>
+			<figure class = "activity3"><img src="images/back4.jpg" alt="Jaafar Hadab"></figure>
+		</section>
+
+		<section class = "contact-info">
+			<h2>Contact</h2>
+			<p>If you'd like to contact me, send me an email at email@gmail.com, or call me on 555-555-5555.</p>
+		</section>
+		
+		<footer>
+			<p>CSC 174: Advanced Front-end Web Design and Development -<a href="http://csc174.org/lab01/jalhadab/" target=_blank>Original Lab 01</a></p>
+		</footer>
+
+	</div>
+
+	<?php include "inc/scripts.php"; ?>
+</body>
+
+</html>
