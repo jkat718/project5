7a2b1a8ef80b74b9da38dce2899d09a39fcc75d2
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-12 23:02:51 -0400

    added gitignore, removed h3 headers to make content more uniform across pages

--- jh.php
+++ jh.php
@@ -3,54 +3,54 @@
 <body class="jaafar">
 
 	<?php include "inc/nav.php"; ?>
-<div class="viewport">
-	<div class="background1">
-		<div class = "container1">
-			<header>
-				<figure class="personal-image"><img src="images/back1_resized.jpg" alt="Jaafar Hadab"></figure>
-				<h1>Jaafar Hadab</h1>
-			</header>
+	<div class="viewport">
+		<div class="background1">
+			<div class = "container1">
+				<header>
+					<figure class="personal-image"><img src="images/back1.jpg" alt="Jaafar Hadab"></figure>
+					<h1>Jaafar Hadab</h1>
+				</header>
 
-			<div class="intro-statement">
-				<p>I am a Mechanical Engineering Student at the University of Rochester. I am experienced in CAD modeling, Finite Element Analysis (FEA), and Manufacturing. I am also minoring in Computer Science and Psychology, from which I received experience in fundemental coding in Java and Python.</p>
+				<div class="intro-statement">
+					<p>I am a Mechanical Engineering Student at the University of Rochester. I am experienced in CAD modeling, Finite Element Analysis (FEA), and Manufacturing. I am also minoring in Computer Science and Psychology, from which I received experience in fundemental coding in Java and Python.</p>
+				</div>
 			</div>
 		</div>
+
+		<h2>Interests</h2>
+
+		<div class="container2">
+			<section class = "extracurricular1">
+				<!-- <h3>Baja SAE</h3> -->
+				<p>I am a member of the University of Rochester Baja SAE team. Each year, we design and build an offroad vehicle to compete with other baja teams from all around the world. I currently serve as the suspension team.</p>
+				<figure class = "activity2"><img src="images/back3_resized.jpg" alt="Doing Baja"></figure>
+			</section>
+
+			<section class = "extracurricular2">
+			<!-- <h3>Research</h3> -->
+				<p>I currently work with the Dias laboratory group in the University of Rochester. The Dias group uses Diamond Anvil Cells (DACs) to study matter at extreme states. My current work with the group involves using Finite Element Models to study the shapes of the tips of diamonds in DACs in order to optimize for highest possible pressure.</p>
+				<figure class = "activity1"><img src="images/back2_resized.jpg" alt="Doing Research"></figure>
+			</section>
+			
+			<section class = "extra-interest">
+				<!-- <h3>Personality Psychology</h3> -->
+				<p>I also have a keen interest in personality psychology, psychodynamic theories, and theories of development, and I am interested in how we can apply these concepts to our everyday lives.</p>
+				<figure class = "activity3"><img src="images/back4_resized.jpg" alt="Conversing"></figure>
+			</section>
+
+			<section class = "contact-info">
+				<h3>Contact</h3>
+				<p>If you'd like to contact me, send me an email at email@gmail.com, or call me on 555-555-5555.</p>
+			</section>
+			
+			<footer>
+				<p>CSC 174: Advanced Front-end Web Design and Development -<a href="http://csc174.org/lab01/jalhadab/" target=_blank>Original Lab 01</a></p>
+			</footer>
+
+		</div>
+
+		<?php include "inc/scripts.php"; ?>
 	</div>
-
-	<h2>Interests</h2>
-
-	<div class="container2">
-		<section class = "extracurricular1">
-			<h3>Baja SAE</h3>
-			<p>I am a member of the University of Rochester Baja SAE team. Each year, we design and build an offroad vehicle to compete with other baja teams from all around the world. I currently serve as the suspension team.</p>
-			<figure class = "activity2"><img src="images/back3_resized.jpg" alt="Doing Baja"></figure>
-		</section>
-
-		<section class = "extracurricular2">
-		<h3>Research</h3>
-			<p>I currently work with the Dias laboratory group in the University of Rochester. The Dias group uses Diamond Anvil Cells (DACs) to study matter at extreme states. My current work with the group involves using Finite Element Models to study the shapes of the tips of diamonds in DACs in order to optimize for highest possible pressure.</p>
-			<figure class = "activity1"><img src="images/back2_resized.jpg" alt="Doing Research"></figure>
-		</section>
-		
-		<section class = "extra-interest">
-			<h3>Personality Psychology</h3>
-			<p>I also have a keen interest in personality psychology, psychodynamic theories, and theories of development, and I am interested in how we can apply these concepts to our everyday lives.</p>
-			<figure class = "activity3"><img src="images/back4_resized.jpg" alt="Conversing"></figure>
-		</section>
-
-		<section class = "contact-info">
-			<h3>Contact</h3>
-			<p>If you'd like to contact me, send me an email at email@gmail.com, or call me on 555-555-5555.</p>
-		</section>
-		
-		<footer>
-			<p>CSC 174: Advanced Front-end Web Design and Development -<a href="http://csc174.org/lab01/jalhadab/" target=_blank>Original Lab 01</a></p>
-		</footer>
-
-	</div>
-
-	<?php include "inc/scripts.php"; ?>
-</div>
 </body>
 
 </html>
--- jm.php
+++ jm.php
@@ -7,7 +7,7 @@
 		<div class="background1">
 			<div class = "container1">
 				<header>
-					<figure class="personal-image"><img src="images/face_resized.jpg" alt="Jack Mandell"></figure>
+					<figure class="personal-image"><img src="images/face.png" alt="Jack Mandell"></figure>
 					<h1>Jack Mandell</h1>
 				</header>
 
@@ -22,19 +22,19 @@
 		<div class="container2">
 
 			<section class = "extracurricular1">
-				<h3>Art</h3>
+				<!-- <h3>Art</h3> -->
 				<p>For many years as a child, I took art lessons, and recently for the past two years over the summer, I have taught art lessons at studios near me. I enjoy painting the most and more specifically I like watercolor and acrylic. Many of my favorite things to paint include landscapes and still life.</p>
 				<figure class = "activity1"><img src="images/art_resized.jpg" alt="art"></figure>
 			</section>
 
 			<section class = "extracurricular2">
-				<h3>Cello</h3>
+				<!-- <h3>Cello</h3> -->
 				<p>I have played cello for many years and enjoy listening to classical music. Wanting to continue playing cello on a consistent basis during college, I joined the University's Chamber Orchestra and have met people who appreciate classical as much as I do. I have even joined a small string ensemble with a few freinds for the upcoming semester.</p>
 				<figure class = "activity2"><img src="images/cello_resized.jpg" alt="cello"></figure>
 			</section>
 
 			<section class = "extra-interest">
-				<h3>Fishing</h3>
+				<!-- <h3>Fishing</h3> -->
 				<p>I love to go fishing in the ponds around my home, as well as exploring new waters when I am traveling. I am drawn to the unpredicatability of fishing and the adventure of exploring new areas I have never fished before. Fishing has taught me a lot about patience and persistence. The feeling of fighting a large fish never gets old.</p>
 				<figure class = "activity3"><img src="images/fishing_resized.jpg" alt="fishing"></figure>
 			</section>
