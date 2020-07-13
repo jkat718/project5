0aca562aba4c76c6f43dbde392ba9af48547c829
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-12 22:14:14 -0400

    Added interest header to each page so theres a common label

--- ar.php
+++ ar.php
@@ -17,28 +17,30 @@
 			</div>
 		</div>
 
+		<h2>Interests</h2>
+
 		<div class="container2">
 
 			<section class = "extracurricular1">
-				<h2>Engineers Without Borders</h2>
+				<h3>Engineers Without Borders</h3>
 				<p>I am a project lead in Engineers Without Borders. My team and I are working on providing clean drinking water to a rural Tanzanian village. Currently, they have to travel 20km each way to a contaminated water source. I have several duties like coordinating meetings, communicating with contacts throughout the world, and writing applications for the national organization.</p>
 				<figure class = "activity2"><img src="images/ewb.jpg" alt="Tanzanian villager"></figure>
 			</section>
 
 			<section class = "extracurricular2">
-				<h2>Volunteering</h2>
+				<h3>Volunteering</h3>
 				<p>I regularly volunteer at the Patchogue Neighbors Soup Kitchen in Patchgue, New York. I work as a line cook, and I started volunteering there in 2012. I cook breakfast for 100+ people, and I coordinate volunteers from the community and local school district.</p>
 				<figure class = "activity1"><img src="images/soupkit.jpg" alt="Soup kitchen"></figure>
 			</section>
 
 			<section class = "extra-interest">
-				<h2>Artificial Intellegince</h2>
+				<h3>Artificial Intellegince</h3>
 				<p>I'm interested in computer vision, security, and machine learning - and how we can use these technologies to help people in their everyday life and their jobs.</p>
 				<figure class = "activity3"><img src="images/ai.jpg" alt="machine learning"></figure>
 			</section>
 
 			<section class = "contact-info">
-				<h2>Contact</h2>
+				<h3>Contact</h3>
 				<p>If you'd like to contact me, send me an email at aramir10@u.rochester.edu, or you can find me on <a href="https://www.linkedin.com/in/aleram" target=_blank>LinkedIn</a>.</p>
 			</section>
 		
--- jh.php
+++ jh.php
@@ -16,29 +16,30 @@
 			</div>
 		</div>
 	</div>
+
+	<h2>Interests</h2>
+
 	<div class="container2">
 		<section class = "extracurricular1">
-			<h2>Baja SAE</h2>
+			<h3>Baja SAE</h3>
 			<p>I am a member of the University of Rochester Baja SAE team. Each year, we design and build an offroad vehicle to compete with other baja teams from all around the world. I currently serve as the suspension team.</p>
 			<figure class = "activity2"><img src="images/back3.jpg" alt="Doing Baja"></figure>
 		</section>
 
 		<section class = "extracurricular2">
-		<h2>Research</h2>
+		<h3>Research</h3>
 			<p>I currently work with the Dias laboratory group in the University of Rochester. The Dias group uses Diamond Anvil Cells (DACs) to study matter at extreme states. My current work with the group involves using Finite Element Models to study the shapes of the tips of diamonds in DACs in order to optimize for highest possible pressure.</p>
 			<figure class = "activity1"><img src="images/back2.jpg" alt="Doing Research"></figure>
 		</section>
-
-		
 		
 		<section class = "extra-interest">
-			<h2>Personality Psychology</h2>
+			<h3>Personality Psychology</h3>
 			<p>I also have a keen interest in personality psychology, psychodynamic theories, and theories of development, and I am interested in how we can apply these concepts to our everyday lives.</p>
 			<figure class = "activity3"><img src="images/back4.jpg" alt="Conversing"></figure>
 		</section>
 
 		<section class = "contact-info">
-			<h2>Contact</h2>
+			<h3>Contact</h3>
 			<p>If you'd like to contact me, send me an email at email@gmail.com, or call me on 555-555-5555.</p>
 		</section>
 		
