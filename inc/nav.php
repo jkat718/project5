bbf715b6477447eb04c7e569de3073c9dcf775a9
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-12 21:29:12 -0400

    Rearranged page links based on last name

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 <nav class="menu">
     <ul>
       <li><a href="index.php">Home</a></li>
-      <li><a href="ar.php">Alejandro Ramirez</a></li>
       <li><a href="jh.php">Jaafar Hadab</a></li>
       <li><a href="jm.php">Jack Mandell</a></li>
+      <li><a href="ar.php">Alejandro Ramirez</a></li>
     </ul>
  </nav>
