f0dd665282701a903092c2f7acbbd453649d2977
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-12 20:33:12 -0400

    removed unused font import

--- inc/html-top.php
+++ inc/html-top.php
@@ -6,5 +6,4 @@
 	<title>Project 2</title>
 	<link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" href="css/navigation.css">
-	<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Niconne&display=swap" rel="stylesheet">
 </head>
--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 <nav class="menu">
     <ul>
       <li><a href="index.php">Home</a></li>
-      <li><a href="ar.php">Alejandro ramirez</a></li>
-      <li><a href="jh.php">Jaafar hadab</a></li>
-      <li><a href="jm.php">Jack mandell</a></li>
+      <li><a href="ar.php">Alejandro Ramirez</a></li>
+      <li><a href="jh.php">Jaafar Hadab</a></li>
+      <li><a href="jm.php">Jack Mandell</a></li>
     </ul>
  </nav>
