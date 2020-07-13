8cbbc0885fbbefe5cfc395ae4c04334041156b9e
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-12 21:43:01 -0400

    formatting fixes

--- jm.php
+++ jm.php
@@ -3,51 +3,55 @@
 <body class="jack">
 
 	<?php include "inc/nav.php"; ?>
-<div class="viewport">
-	<div class="background1">
-		<div class = "container1">
-			<header>
-				<figure class="personal-image"><img src="images/face.png" alt="Jack Mandell"></figure>
-				<h1>Jack Mandell</h1>
-			</header>
+	<div class="viewport">
+		<div class="background1">
+			<div class = "container1">
+				<header>
+					<figure class="personal-image"><img src="images/face.png" alt="Jack Mandell"></figure>
+					<h1>Jack Mandell</h1>
+				</header>
 
-			<div class="intro-statement">
-				<p>I am a Math and Computer Science double major studying at the University of Rochester. I am a rising sophomore, and outside of my majors, I enjoy passing the time with some of my favorite hobbies. These hobbies include art, cello, and fishing.</p>
+				<div class="intro-statement">
+					<p>I am a Math and Computer Science double major studying at the University of Rochester. I am a rising sophomore, and outside of my majors, I enjoy passing the time with some of my favorite hobbies. These hobbies include art, cello, and fishing.</p>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="container2">
-		<section class = "extracurricular1">
-			<h2>Art</h2>
-			<p>For many years as a child, I took art lessons, and recently for the past two years over the summer, I have taught art lessons at studios near me. I enjoy painting the most and more specifically I like watercolor and acrylic. Many of my favorite things to paint include landscapes and still life.</p>
-			<figure class = "activity1"><img src="images/art.jpg" alt="art"></figure>
-		</section>
 
-		<section class = "extracurricular2">
-			<h2>Cello</h2>
-			<p>I have played cello for many years and enjoy listening to classical music. Wanting to continue playing cello on a consistent basis during college, I joined the University's Chamber Orchestra and have met people who appreciate classical as much as I do. I have even joined a small string ensemble with a few freinds for the upcoming semester.</p>
-			<figure class = "activity2"><img src="images/cello.png" alt="cello"></figure>
-		</section>
+		<div class="container2">
 
-		<section class = "extra-interest">
-			<h2>Fishing</h2>
-			<p>I love to go fishing in the ponds around my home, as well as exploring new waters when I am traveling. I am drawn to the unpredicatability of fishing and the adventure of exploring new areas I have never fished before. Fishing has taught me a lot about patience and persistence. The feeling of fighting a large fish never gets old.</p>
-			<figure class = "activity3"><img src="images/fishing.png" alt="fishing"></figure>
-		</section>
+			<section class = "extracurricular1">
+				<h2>Art</h2>
+				<p>For many years as a child, I took art lessons, and recently for the past two years over the summer, I have taught art lessons at studios near me. I enjoy painting the most and more specifically I like watercolor and acrylic. Many of my favorite things to paint include landscapes and still life.</p>
+				<figure class = "activity1"><img src="images/art.jpg" alt="art"></figure>
+			</section>
 
-		<section class = "contact-info">
-			<h2>Contact</h2>
-			<p>If you'd like to contact me, send me an email at email@gmail.com, or call me on 555-555-5555.</p>
-		</section>
+			<section class = "extracurricular2">
+				<h2>Cello</h2>
+				<p>I have played cello for many years and enjoy listening to classical music. Wanting to continue playing cello on a consistent basis during college, I joined the University's Chamber Orchestra and have met people who appreciate classical as much as I do. I have even joined a small string ensemble with a few freinds for the upcoming semester.</p>
+				<figure class = "activity2"><img src="images/cello.png" alt="cello"></figure>
+			</section>
+
+			<section class = "extra-interest">
+				<h2>Fishing</h2>
+				<p>I love to go fishing in the ponds around my home, as well as exploring new waters when I am traveling. I am drawn to the unpredicatability of fishing and the adventure of exploring new areas I have never fished before. Fishing has taught me a lot about patience and persistence. The feeling of fighting a large fish never gets old.</p>
+				<figure class = "activity3"><img src="images/fishing.png" alt="fishing"></figure>
+			</section>
+
+			<section class = "contact-info">
+				<h2>Contact</h2>
+				<p>If you'd like to contact me, send me an email at email@gmail.com, or call me on 555-555-5555.</p>
+			</section>
 		
-		<footer>
-			<p>CSC 174: Advanced Front-end Web Design and Development -<a href="http://csc174.org/lab01/jmandel5/lab01/" target=_blank>Original Lab 01</a></p>
-		</footer>
+			<footer>
+				<p>CSC 174: Advanced Front-end Web Design and Development -<a href="http://csc174.org/lab01/jmandel5/lab01/" target=_blank>Original Lab 01</a></p>
+			</footer>
 
-	</div>
+		</div>
 
 	<?php include "inc/scripts.php"; ?>
-</div>
+
+	</div>
+	
 </body>
 
 </html>
