8d068121255e554bef705ca6a9683ed8f4ed2eb9
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-12 22:52:39 -0400

    updated image css, resized images

--- jh.php
+++ jh.php
@@ -7,7 +7,7 @@
 	<div class="background1">
 		<div class = "container1">
 			<header>
-				<figure class="personal-image"><img src="images/back1.jpg" alt="Jaafar Hadab"></figure>
+				<figure class="personal-image"><img src="images/back1_resized.jpg" alt="Jaafar Hadab"></figure>
 				<h1>Jaafar Hadab</h1>
 			</header>
 
@@ -23,19 +23,19 @@
 		<section class = "extracurricular1">
 			<h3>Baja SAE</h3>
 			<p>I am a member of the University of Rochester Baja SAE team. Each year, we design and build an offroad vehicle to compete with other baja teams from all around the world. I currently serve as the suspension team.</p>
-			<figure class = "activity2"><img src="images/back3.jpg" alt="Doing Baja"></figure>
+			<figure class = "activity2"><img src="images/back3_resized.jpg" alt="Doing Baja"></figure>
 		</section>
 
 		<section class = "extracurricular2">
 		<h3>Research</h3>
 			<p>I currently work with the Dias laboratory group in the University of Rochester. The Dias group uses Diamond Anvil Cells (DACs) to study matter at extreme states. My current work with the group involves using Finite Element Models to study the shapes of the tips of diamonds in DACs in order to optimize for highest possible pressure.</p>
-			<figure class = "activity1"><img src="images/back2.jpg" alt="Doing Research"></figure>
+			<figure class = "activity1"><img src="images/back2_resized.jpg" alt="Doing Research"></figure>
 		</section>
 		
 		<section class = "extra-interest">
 			<h3>Personality Psychology</h3>
 			<p>I also have a keen interest in personality psychology, psychodynamic theories, and theories of development, and I am interested in how we can apply these concepts to our everyday lives.</p>
-			<figure class = "activity3"><img src="images/back4.jpg" alt="Conversing"></figure>
+			<figure class = "activity3"><img src="images/back4_resized.jpg" alt="Conversing"></figure>
 		</section>
 
 		<section class = "contact-info">
