4b601c29ada81cfbc0a53d9fde0d177b2fad08f8
Bonnie Yang <bonniehyang@gmail.com>
2020-07-15 19:47:57 -0400

    Update list.php

---
+++ list.php
@@ -0,0 +1,10 @@
+<?php include "inc/html-top.php"; ?>
+<body>
+  <a href="index.php">Home</a>
+  <footer>
+    <p>CSC 174: Advanced Front-end Web Design and Development - <a href="http://docs.csc174.org" target=_blank>Course home page</a></p>
+  </footer>
+
+</body>
+
+</html>
