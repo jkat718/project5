bd6ca9fb41d48c8ea7a6fa57ce74ecd1557bebdd
Bonnie Yang <bonniehyang@gmail.com>
2020-07-17 15:47:09 -0400

    added headers to new pages

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include "inc/html-top.php"; ?>
 <body>
+<header>CSC 174 (Fall 2020)</header>
 
 <footer>
   CSC 174: Advanced Front-end Web Design and Development - <a href="http://csc174.org/lab01/aramir10/" target=_blank>Original Lab 01</a>
--- list.php
+++ list.php
@@ -1,6 +1,8 @@
 <?php include "inc/html-top.php"; ?>
 <body>
-  <a href="index.php">Home</a>
+  <header>CSC 174 (FALL 2020)
+  <a href="index.php">< Home</a>
+  </header>
   <footer>
     CSC 174: Advanced Front-end Web Design and Development - <a href="http://docs.csc174.org" target=_blank>Course home page</a>
   </footer>
