e8ca0ae7d2775b5bcf1c0aaa7787db016fa96502
Bonnie Yang <bonniehyang@gmail.com>
2020-07-17 13:46:39 -0400

    moved footer to bottom of page

--- ar.php
+++ ar.php
@@ -45,14 +45,13 @@
 			</section>
 
 		</div>
-
-    <footer>
-        <p>CSC 174: Advanced Front-end Web Design and Development - <a href="http://csc174.org/lab01/aramir10/" target=_blank>Original Lab 01</a></p>
-    </footer>
-
 	<?php include "inc/scripts.php"; ?>
 
 	</div>
+
+  <footer>
+    <p>CSC 174: Advanced Front-end Web Design and Development - <a href="http://csc174.org/lab01/aramir10/" target=_blank>Original Lab 01</a></p>
+  </footer>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -29,16 +29,14 @@
 					</ul>
 			</article>
 
-		<footer>
-			<p>
-			CSC 174: Advanced Front-end Web Design and Development -
-			<a href="http://docs.csc174.org" target=_blank>Course home page</a>
-			</p>
-		</footer>
-
 		</div>
 		<?php include "inc/scripts.php"; ?>
 	</div>
+
+<footer>
+  <p>CSC 174: Advanced Front-end Web Design and Development - <a href="http://csc174.org/lab01/aramir10/" target=_blank>Original Lab 01</a></p>
+</footer>
+
 </body>
 
 </html>
