24a84e5a7e47272e955e24d60d798833e4c333fb
Bonnie Yang <bonniehyang@gmail.com>
2020-07-17 14:02:40 -0400

    emptied index for z-comp

--- index.php
+++ index.php
@@ -1,38 +1,6 @@
 <?php include "inc/html-top.php"; ?>
 <body>
 
-	<?php include "inc/nav.php"; ?>
-	<div class="viewport">
-		<div class="container">
-			<article class="homepage">
-				<h1 class="welcome">welcome to our page!</h1>
-					<ul class="home">
-
-						<li class="jh">
-							<h2>Jaafar Hadab</h2>
-							<figure class="intrf"><img src="images/back1.jpg" alt="Jaafar Hadab"><figcaption>Jaafar Hadab</figcaption></figure>
-							<p class="intrp">Baja - Research - Psychology</p><a href="jh.php" class="rdmore">Read more</a>
-						</li>
-
-						<li class="jm">
-							<h2>Jack Mandell</h2>
-							<figure class="intrf"><img src="images/face.png" alt="Jack Mandell"><figcaption>Jack Mandell</figcaption></figure>
-							<p class="intrp">Art - Cello - Fishing</p><a href="jm.php" class="rdmore">Read more</a>
-						</li>
-
-						<li class="am">
-							<h2>Alejandro Ramirez</h2>
-							<figure class="intrf"><img src="images/met_resized.jpg" alt="Alejandro Ramirez"><figcaption>Alejandro Ramirez</figcaption></figure>
-							<p class="intrp">AI - EWB - Volunteering</p><a href="ar.php" class="rdmore">Read more</a>
-						</li>
-
-					</ul>
-			</article>
-
-		</div>
-		<?php include "inc/scripts.php"; ?>
-	</div>
-
 <footer>
   <p>CSC 174: Advanced Front-end Web Design and Development - <a href="http://csc174.org/lab01/aramir10/" target=_blank>Original Lab 01</a></p>
 </footer>
