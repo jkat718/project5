93cebd4ebf450939052d62b196cfc9691a9e4a27
pseudoCalibrator <leahgoodwin10@gmail.com>
2020-07-18 05:15:41 -0400

    f-pattern implemented

--- list.php
+++ list.php
@@ -6,37 +6,51 @@
       < Home</a> </header> <div class="viewport">
         <h1>Student Index</h1>
         <h2>Team New York</h2>
-        <section>
-          <img src="images/back1.jpg" alt="Jaafar Hadab">
-          <h3>Jaafar Hadab</h3>
-          <p>I am a Mechanical Engineering Student at the University of Rochester.
-            I am experienced in CAD modeling, Finite Element Analysis (FEA), and Manufacturing.
-            I am also minoring in Computer Science and Psychology, from which I received
-            experience in fundemental coding in Java and Python.
-          </p>
-          <a href="jh.php">Personal Page</a>
-        </section>
 
-        <section>
-          <img src="images/face.png" alt="Jack Mandell">
-          <h3>Jack Mandall</h3>
-          <p>I am a Math and Computer Science double major studying at the University of Rochester.
-            I am a rising sophomore, and outside of my majors, I enjoy passing the time with some
-            of my favorite hobbies. These hobbies include art, cello, and fishing.</p>
-          <a href="jm.php">Personal Page</a>
-        </section>
+        <div class="grid-container">
+          <section class="row1">
+            <div class="grid-container">
+              <img src="images/back1.jpg" alt="Jaafar Hadab">
+              <div class="column2">
+                <h3>Jaafar Hadab</h3>
+                <p>I am a Mechanical Engineering Student at the University of Rochester.
+                  I am experienced in CAD modeling, Finite Element Analysis (FEA), and Manufacturing.
+                  I am also minoring in Computer Science and Psychology, from which I received
+                  experience in fundemental coding in Java and Python.
+                </p>
+              </div>
 
-        <section>
-          <img src="images/met_resized.jpg" alt="Alejandro Ramirez">
-          <h3>Alejandro Ramirez</h3>
-          <p>I'm a rising junior from Long Island, New York. I'm majoring in Computer Science
-            and minoring in Digital Media Studies. Before UR, I spent the first semester of my
-            college career at RPI studying Mechanical Engineering. Aside from computing, I love
-            cooking, volunteering, hiking/the outdoors, and music!</p>
-          <a href="ar.php">Personal Page</a>
-        </section>
+              <a href="jh.php">Personal Page</a>
+            </div>
+          </section>
 
+          <section class="row2">
+            <div class="grid-container">
+              <img src="images/face.png" alt="Jack Mandell">
+              <div class="column2">
+                <h3>Jack Mandall</h3>
+                <p>I am a Math and Computer Science double major studying at the University of Rochester.
+                  I am a rising sophomore, and outside of my majors, I enjoy passing the time with some
+                  of my favorite hobbies. These hobbies include art, cello, and fishing.</p>
+              </div>
 
+              <a href="jm.php">Personal Page</a>
+            </div>
+          </section>
+
+          <section class="row3">
+            <div class="grid-container">
+            <img src="images/met_resized.jpg" alt="Alejandro Ramirez">
+            <div class="column2">
+            <h3>Alejandro Ramirez</h3>
+            <p>I'm a rising junior from Long Island, New York. I'm majoring in Computer Science
+              and minoring in Digital Media Studies. Before UR, I spent the first semester of my
+              college career at RPI studying Mechanical Engineering. Aside from computing, I love
+              cooking, volunteering, hiking/the outdoors, and music!</p></div>
+            <a href="ar.php">Personal Page</a></div>
+          </section>
+
+        </div>
         </div>
 
 
