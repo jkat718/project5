2eac4e32dd9ec554c12690cddf3b32ff10c898dc
Bonnie Yang <bonniehyang@gmail.com>
2020-07-18 08:41:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php include "inc/html-top.php"; ?>
 <body>
-<header>CSC 174 (Fall 2020)</header>
+<header>CSC 174 (FALL 2020)</header>
 
-<div class="slider">
-  <img src="images/cover.jpg" alt="cover">
-  <img src="images/ar.jpg" alt="ar website">
-  <img src="images/jh.jpg" alt="jh website">
-  <img src="images/jm.jpg" alt="jm website">
+<div>
+  <div class="slider">
+    <img src="images/cover.jpg" alt="cover">
+    <img src="images/ar.jpg" alt="ar website">
+    <img src="images/jh.jpg" alt="jh website">
+    <img src="images/jm.jpg" alt="jm website">
+  </div>
+
+  <form></form>
+
+  <article>
+    <h2>About CSC 174</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, minima quidem! Quidem officia ipsam nisi repudiandae aut quia, non dolores. Quia nobis, eaque inventore illum enim, nisi quasi laboriosam eius!</p>
+  </article>
+
+  <div><a href="list.php">Head to the student index ></a></div>
 </div>
-
-<article>
-  <h2>About CSC 174</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, minima quidem! Quidem officia ipsam nisi repudiandae aut quia, non dolores. Quia nobis, eaque inventore illum enim, nisi quasi laboriosam eius!</p>
-</article>
-
-<div><a href="list.php">Head to the student index ></a></div>
 
 <footer>
   CSC 174: Advanced Front-end Web Design and Development
