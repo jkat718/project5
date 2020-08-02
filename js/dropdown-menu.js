1b1e5b5bb8a10c8cecd6d79933f8d3a531f6a155
jalhadab <67709677+jalhadab@users.noreply.github.com>
2020-08-02 18:26:16 +0300

    got rid of dead code in dropdown-menu.js

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -2,13 +2,13 @@
 function myFunction(ident) {
   document.getElementById(ident).classList.toggle("show");
 }
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var elements = document.getElementsByClassName("dropbtn");
-    for (var i = 0, len = elements.length; i<len; i++){
-      if (elements[i].classList.contains('show')) {
-        elemments[i].classList.remove('show');
-      }
-    }
-  }
-}
+// window.onclick = function(e) {
+//   if (!e.target.matches('.dropbtn')) {
+//     var elements = document.getElementsByClassName("dropbtn");
+//     for (var i = 0, len = elements.length; i<len; i++){
+//       if (elements[i].classList.contains('show')) {
+//         elemments[i].classList.remove('show');
+//       }
+//     }
+//   }
+// }
